9c9dcb86e60e465ff8d83fd589b4125bbcaa19d2
joy2fun <web@css.js.cn>
2016-04-19 09:18:07 +0800

    inotify

--- sh/setup.sh
+++ sh/setup.sh
@@ -36,7 +36,7 @@ command_exists(){
 
 # install ncurses & fontconfig
 if command_exists $aptget; then
-    for packages in fontconfig gcc make zsh libc6-dbg gdb libncurses5-dev libcurl-dev apache2-utils lrzsz;
+    for packages in fontconfig gcc make zsh libc6-dbg gdb libncurses5-dev libcurl-dev apache2-utils lrzsz inotify-tools;
     do sudo apt-get install -y $packages; done
 elif command_exists $yum; then
     for packages in ncurses-devel fontconfig curl-devel;
