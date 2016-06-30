519d5e0008a2f53db5c217db58d47abed8f674d2
joy2fun <web@css.js.cn>
2016-06-30 16:56:25 +0800

    4windows

--- libs/alias.sh
+++ libs/alias.sh
@@ -12,12 +12,9 @@ alias targz='tar zxf'
 
 alias psg='ps aux|grep '
 alias hist='cat ~/.zsh_history | grep '
-# list
-alias lh='ls -lhA'
 
 alias v='vim -u ~/.vim/vimrc'
 alias vv='sudo vim -u ~/.vim/vimrc'
-alias gpo='git push origin'
 
 alias mc='valgrind --tool=memcheck'
 
@@ -38,12 +35,35 @@ if [ $(uname -s) = "Darwin" ] ; then
     alias gh='open https://github.com/'
     alias java='/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin/java'
 else
-    iscmd apt-get && {
-        alias agi='sudo apt-get install '
-        alias acs='apt-cache search '
-    }
+    alias agi='sudo apt-get install '
+    alias acs='apt-cache search '
 fi
 
 alias cpmakefile='cp ~/dotfiles/Makefile ./'
 alias cs='~/dotfiles/bin/code-snippet.sh'
 
+# common
+alias t='tail -f'
+
+alias l='ls -lFh'     #size,show type,human readable
+alias la='ls -lAFh'   #long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
+alias ll='ls -l'      #long list
+alias ldot='ls -ld .*'
+alias lh='ls -lhA'
+
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+# git
+alias g='git'
+alias gd='git diff'
+alias gp='git push'
+alias gl='git pull'
+alias gpo='git push origin'
+alias gst='git status'
+alias gcp='git cherry-pick'
+alias gcam='git commit -a -m'
+alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
