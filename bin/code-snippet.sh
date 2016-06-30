d6c19cf0bb65c0fcecfdc117bdcc58644f68d4eb
joy2fun <web@css.js.cn>
2016-06-30 15:53:48 +0800

    code snippet tool

--- bin/code-snippet.sh
+++ bin/code-snippet.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-gitquiet="--quiet"
-gitquiet=""
-gitbin=$(which git)
-
 csurl="git@git.oschina.net:home5u/code-snippets.git"
 cspath=~/code-snippets
 
 cmd=${1}
 path=${2}
 
-[ -z "$cmd" ] {
-    echo "Usage: $0 {get|add|edit} {path}"
+cwd=$(pwd)
+helpmsg="Usage: $0 {get|add|delete} {path}"
+gitquiet="--quiet"
+gitbin=$(which git)
+
+[ -z "$cmd" ] && {
+    echo $helpmsg
     exit 1
 }
 
 [ ! -d "$cspath" ] && {
-    $gitbin clone $csurl $cspath
+    $gitbin clone $csurl $cspath $gitquiet
 }
 
 [ -d "$cspath" ] && {
@@ -25,7 +26,52 @@ path=${2}
         echo "[$cspath] is not a git dir."
         exit 1
     }
-    $gitbin clean -n -d -f $gitquiet
-    $gitbin pull origin master
+    $gitbin clean -n -d -f $gitquiet $gitquiet
+    $gitbin pull origin master $gitquiet >/dev/null
 }
 
+case $cmd in
+    get )
+        [ -z "$path" ] && {
+            echo $helpmsg
+            exit 1
+        }
+
+        if [ -f "$cspath/$path" ] ; then
+            cat "$cspath/$path"
+        elif [ -d "$cspath/$path" ] ; then
+            cp -i -r "$cspath/$path" -t "$cwd"
+        else
+            echo "Remote not Found!"
+            exit 1
+        fi
+
+    ;;
+
+    add )
+        if [ -e "$cwd/$path" ] ; then
+            cp -rf "$cwd/$path" "$cspath"
+        else
+            echo "invalid path."
+            exit 1
+        fi
+
+        $gitbin add -A > /dev/null
+        $gitbin commit -a -m "ignore" $gitquiet >/dev/null
+        $gitbin push origin master $gitquiet
+    ;;
+
+    delete )
+        [ -e "$cspath/$path" ] && {
+            rm -rf "$cspath/$path"
+            $gitbin commit -a -m "ignore" $gitquiet >/dev/null
+            $gitbin push origin master $gitquiet
+        }
+
+    ;;
+
+    * )
+        echo "unkonw cmd."
+        echo $helpmsg
+        exit 1
+esac
