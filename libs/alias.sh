5c317544346b452e69f874a87a544398a087f008
joy2fun <web@css.js.cn>
2016-11-28 21:02:22 +0800

    alias

--- libs/alias.sh
+++ libs/alias.sh
@@ -69,3 +69,5 @@ alias gst='git status'
 alias gcp='git cherry-pick'
 alias gcam='git commit -a -m'
 alias glol="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+alias vg='vagrant'
